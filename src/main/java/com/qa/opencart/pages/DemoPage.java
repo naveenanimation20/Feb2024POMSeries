ec927bad49c0979153954461b83d684b5060fa42
Naveen AutomationLabs <naveenanimation20@gmail.com>
2024-07-01 07:26:12 +0530

    updated demopage as per NaveenTL review comments

--- src/main/java/com/qa/opencart/pages/DemoPage.java
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -5,9 +5,18 @@ import org.openqa.selenium.By;
 public class DemoPage {
 
 	By demo = By.id("demo");
+	By cart = By.id("cart");
+
+	
 
 	public void getDemo() {
 		System.out.println(demo);
+		System.out.println("demo details");
 	}
 
+	public void addtoCart() {
+		System.out.println(cart);
+		System.out.println("cart details");
+	}
+	
 }
