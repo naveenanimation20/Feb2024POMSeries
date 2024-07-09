d146657dfa2ccfc68fe8d0729e29203795ff28c9
Naveen AutomationLabs <naveenanimation20@gmail.com>
2024-07-09 07:11:58 +0530

    fixed the login title

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -13,7 +13,7 @@ public class AppConstants {
 	public static final String CONFIG_UAT_FILE_PATH = "./src/test/resources/config/uat.properties";
 	public static final String CONFIG_STAGE_FILE_PATH = "./src/test/resources/config/stage.properties";
 	
-	public static final String LOGIN_PAGE_TITLE = "Account Login11";
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String ACCOUNTS_PAGE_TITLE = "My Account";
 
 	public static final String LOGIN_PAGE_FRACTION_URL = "route=account/login";
